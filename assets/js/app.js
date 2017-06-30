390273d04b74709ec1d33b1f690b1429378e2a73
kc657 <kvn.0218@gmail.com>
2017-06-30 08:56:41 -0700

    Added hover in JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,15 @@
 // sanity check
 console.log("app.js linked.");
+$(document).ready(function(){
+
+// $(window).on('scroll', function scrolling(){
+// $('body').append("<p>asdfljal;skdf</p>")
+// });
+
+$("a").hover(function(){
+  $ (this).toggleClass("toplinks_hover");
+})
+
+;
+
+});
